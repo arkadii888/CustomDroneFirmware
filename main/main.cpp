612625646899ed480e116aa55c6ddc4941d64195
arkadii <arkadii.personal@proton.me>
2026-04-12 22:43:09 +0200

    spi bus + BMI270 init

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/i2c_master.h"
+#include "driver/spi_master.h"
 #include "Battery.h"
 
 extern "C" void app_main(void)
@@ -29,6 +30,27 @@ extern "C" void app_main(void)
     i2c_master_dev_handle_t dev_handle;
     ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
     Battery battery(dev_handle);
+
+    // SPI Init
+    spi_bus_config_t buscfg = {};
+    buscfg.miso_io_num = 43;
+    buscfg.mosi_io_num = 14;
+    buscfg.sclk_io_num = 44;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = 0;
+
+    ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+    // BMI270(Accelerometer + Gyroscope) Init
+    spi_device_interface_config_t devcfg = {};
+    devcfg.clock_speed_hz = 1000000;
+    devcfg.mode = 0;
+    devcfg.spics_io_num = 46;
+    devcfg.queue_size = 7;
+
+    spi_device_handle_t spi_handle;
+    ESP_ERROR_CHECK(spi_bus_add_device(SPI2_HOST, &devcfg, &spi_handle));
 
     std::cout << "Init finished!" << std::endl;
 
