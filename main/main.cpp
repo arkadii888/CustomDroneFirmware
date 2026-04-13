e713eb26d9355de570a8f574c940160460f552d2
arkadii <arkadii.personal@proton.me>
2026-04-13 23:08:48 +0200

    bmi270 config upload

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdint>
 #include <iostream>
+#include <string.h>
 #include <sys/types.h>
+#include "esp_heap_caps.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/spi_master.h"
@@ -8,6 +10,7 @@
 
 #include "I2C.h"
 #include "Battery.h"
+#include "bmi270_config.h"
 
 extern "C" void app_main(void)
 {
@@ -23,7 +26,7 @@ extern "C" void app_main(void)
     buscfg.sclk_io_num = 44;
     buscfg.quadwp_io_num = -1;
     buscfg.quadhd_io_num = -1;
-    buscfg.max_transfer_sz = 0;
+    buscfg.max_transfer_sz = 8193;
 
     ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO));
 
@@ -57,11 +60,73 @@ extern "C" void app_main(void)
         std::cout << "BMI270 check error!" << std::endl;
     }
 
+    // Disable BMI270 Powersave
+    spi_transaction_t t2 = {};
+    t2.length = 16;
+    t2.tx_data[0] = 0x7C & 0x7F;
+    t2.tx_data[1] = 0x00;
+    t2.flags = SPI_TRANS_USE_TXDATA;
+
+    auto err2 = spi_device_transmit(bmi270_handle, &t2);
+    std::cout << "Disable BMI270 Powersave: " << esp_err_to_name(err2) << std::endl;
+    vTaskDelay(1 / portTICK_PERIOD_MS);
+
+    // BMI270 Prepare For Config
+    spi_transaction_t t3 = {};
+    t3.length = 16;
+    t3.tx_data[0] = 0x59 & 0x7F;
+    t3.tx_data[1] = 0x00;
+    t3.flags = SPI_TRANS_USE_TXDATA;
+
+    auto err3 = spi_device_transmit(bmi270_handle, &t3);
+    std::cout << "BMI270 Prepare For Config: " << esp_err_to_name(err3) << std::endl;
+
+    // BMI270 Load Config
+    uint8_t* buffer = static_cast<uint8_t*>(heap_caps_malloc(8193, MALLOC_CAP_DMA));
+    memcpy(buffer, bmi270_config_file, 8193);
+
+    spi_transaction_t t4 = {};
+    t4.length = 8193 * 8;
+    t4.tx_buffer = buffer;
+
+    auto err4 = spi_device_transmit(bmi270_handle, &t4);
+    free(buffer);
+
+    std::cout << "BMI270 Load Config: " << esp_err_to_name(err4) << std::endl;
+
+    // BMI270 Unpack Config
+    spi_transaction_t t5 = {};
+    t5.length = 16;
+    t5.tx_data[0] = 0x59 & 0x7F;
+    t5.tx_data[1] = 0x01;
+    t5.flags = SPI_TRANS_USE_TXDATA;
+
+    auto err5 = spi_device_transmit(bmi270_handle, &t5);
+    std::cout << "BMI270 Unpack Config: " << esp_err_to_name(err5) << std::endl;
+    vTaskDelay(150 / portTICK_PERIOD_MS);
+
+    // Check Config Load Status
+    spi_transaction_t t6 = {};
+    t6.length = 24;
+    t6.tx_data[0] = 0x21 | 0x80;
+    t6.tx_data[1] = 0x00;
+    t6.tx_data[2] = 0x00;
+    t6.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA;
+
+    auto err6 = spi_device_transmit(bmi270_handle, &t6);
+    std::cout << "Check Config Load Status: " << esp_err_to_name(err6) << std::endl;
+
+    std::cout << "BMI270 Config Load Answer: 0x" << std::hex << static_cast<int>(t6.rx_data[2]) << std::dec << std::endl;
+
+    if(t6.rx_data[2] == 0x01) {
+        std::cout << "BMI270 Config Load Success!" << std::endl;
+    }
+
     std::cout << "Init finished!" << std::endl;
 
     while (true) {
         std::cout << "Battery voltage: " << battery.GetVoltage() << " V" << std::endl;
         std::cout << "Battery current: " << battery.GetCurrent() << " A" << std::endl;
-        vTaskDelay(500/ portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
