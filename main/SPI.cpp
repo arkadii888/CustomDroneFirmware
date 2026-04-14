91e877f5e27347443c0658cbedd0b6e81fb9093d
arkadii <arkadii.personal@proton.me>
2026-04-14 13:45:41 +0200

    Update SPI.cpp

--- main/SPI.cpp
+++ main/SPI.cpp
@@ -1,13 +1,13 @@
 #include "SPI.h"
 
 SPI::SPI() {
-    spi_bus_config_t buscfg = {};
-    buscfg.miso_io_num = 43;
-    buscfg.mosi_io_num = 14;
-    buscfg.sclk_io_num = 44;
-    buscfg.quadwp_io_num = -1;
-    buscfg.quadhd_io_num = -1;
-    buscfg.max_transfer_sz = 8193;
+    spi_bus_config_t config = {};
+    config.miso_io_num = 43;
+    config.mosi_io_num = 14;
+    config.sclk_io_num = 44;
+    config.quadwp_io_num = -1;
+    config.quadhd_io_num = -1;
+    config.max_transfer_sz = 8193;
 
-    ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &config, SPI_DMA_CH_AUTO));
 }
